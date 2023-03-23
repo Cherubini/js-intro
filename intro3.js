feb5dc552aa8f3a180e85b5d6384f50084e1a8f3
Cherubini <127298157+Cherubini@users.noreply.github.com>
2023-03-23 13:10:36 +0100

    eserc

---
+++ intro3.js
@@ -0,0 +1,50 @@
+// 
+//FUNZIONI-----------------------------------------
+
+// function elevaAlQuadrato(numero)
+//     {
+//     const risultato = numero * numero;
+//     return risultato;
+//     }   
+
+// const dueAlQuadrato = elevaAlQuadrato(2);
+// console.log(dueAlQuadrato);
+
+// const cinqueAlQuadrato = elevaAlQuadrato(5);
+
+// console.log(cinqueAlQuadrato);
+// console.log(cinqueAlQuadrato);
+// console.log(elevaAlQuadrato(50));
+
+// //funzioni anonime
+
+// const elevaAlQuadrato2 = function(numero){
+//     const risultato = numero * numero;
+//     return risultato;
+// }
+// console.log(elevaAlQuadrato2((3)));
+
+// //funziono freccia
+
+// const elevaAlQuadrato3 = (numero) => numero * numero;
+
+// console.log(elevaAlQuadrato3(12));
+
+// function elevazioneAPotenza(base, esponente) {
+//     const risultato= base**esponente;
+//     return risultato;
+// }
+
+// console.log(elevazioneAPotenza(3,4));
+
+//funzioni pure e impure--------------------------------
+
+// function scriviNellaconsole(messaggio)
+// {
+// console.log(messaggio)
+// }
+
+// scriviNellaconsole('scemo chi legge');
+// function sommaDueELogga(numeo) {
+//       6
+// }
